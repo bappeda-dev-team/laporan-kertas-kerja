38723de6439d04cf77035f4f10315c01774f01bf
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-10-16 15:00:16 +0700

    get nama atasan dari list verifikator

--- src/main/java/cc/kertaskerja/laporan/repository/VerifikatorRepository.java
+++ src/main/java/cc/kertaskerja/laporan/repository/VerifikatorRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface VerifikatorRepository extends JpaRepository<Verifikator, Long> {
+    List<Verifikator> findByNipAndTahunVerifikasi(String nip, String tahunVerifikasi);
 
     @Query("SELECT v FROM Verifikator v WHERE v.nip = :nip")
     List<Verifikator> findVerifikatorByNip(@Param("nip") String nip);
--- src/main/java/cc/kertaskerja/laporan/service/cetak/RencanaKinerjaAtasanService.java
+++ src/main/java/cc/kertaskerja/laporan/service/cetak/RencanaKinerjaAtasanService.java
@@ -3,8 +3,10 @@ package cc.kertaskerja.laporan.service.cetak;
 import cc.kertaskerja.laporan.dto.cetak.RencanaKinerjaHierarchyDTO;
 import cc.kertaskerja.laporan.dto.cetak.RencanaKinerjaHierarchyResponse;
 import cc.kertaskerja.laporan.entity.RencanaKinerjaAtasan;
+import cc.kertaskerja.laporan.entity.Verifikator;
 import cc.kertaskerja.laporan.helper.Crypto;
 import cc.kertaskerja.laporan.repository.RencanaKinerjaAtasanRepository;
+import cc.kertaskerja.laporan.repository.VerifikatorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,8 +17,9 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class RencanaKinerjaAtasanService {
     private final RencanaKinerjaAtasanRepository repository;
+    private final VerifikatorRepository verifikatorRepository;
 
-    public RencanaKinerjaHierarchyResponse getHierarchyByNipAndTahun(String encryptedNip) {
+    public RencanaKinerjaHierarchyResponse getHierarchyByNipAndTahun(String encryptedNip, String tahun_verifikasi) {
         String decryptedNip = Crypto.decrypt(encryptedNip);
 
         List<RencanaKinerjaAtasan> list = repository.findByNipBawahan(decryptedNip);
@@ -27,10 +30,18 @@ public class RencanaKinerjaAtasanService {
                     .build();
         }
 
-        // first data for setup nama atasan bawahan
+        // find atasan dari status rencana kinerja
+        // tambah kode opd kalau perlu
+        List<Verifikator> listAtasan = verifikatorRepository.findByNipAndTahunVerifikasi(encryptedNip, tahun_verifikasi);
+        if (listAtasan.size() != 1) {
+            throw new IllegalStateException("Atasan belum dipilih");
+        }
+        Verifikator atasan = listAtasan.getFirst();
+        String namaAtasan = atasan.getNamaAtasan();
+        String nipAtasan = atasan.getNipAtasan();
+
+        // find data for setup bawahan
         RencanaKinerjaAtasan first = list.getFirst();
-        String nipAtasan = first.getNip();
-        String namaAtasan = first.getNama();
         String nipBawahan = first.getNipBawahan();
         String namaBawahan = first.getNamaBawahan();
 
